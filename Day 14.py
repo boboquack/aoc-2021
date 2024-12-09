7066fee984134465b329a226b0db5e35a2cb399e
boboquack <boboquack@users.noreply.github.com>
2024-12-09 06:39:57 +0100

    Remove input data

--- Day 14.py
+++ Day 14.py
@@ -1,104 +1,5 @@
-s='OFSVVSFOCBNONHKFHNPK'
-t='''HN -> C
-VB -> K
-PF -> C
-BO -> F
-PB -> F
-OH -> H
-OB -> N
-PN -> O
-KO -> V
-CK -> V
-FP -> H
-PC -> V
-PP -> N
-FN -> N
-CC -> F
-FC -> N
-BP -> N
-SH -> F
-NS -> V
-KK -> B
-HS -> C
-NV -> N
-FO -> B
-VO -> S
-KN -> F
-SC -> V
-NB -> H
-CH -> B
-SF -> V
-NP -> V
-FB -> P
-CV -> B
-PO -> P
-SV -> P
-OO -> V
-PS -> C
-CO -> N
-SP -> B
-KP -> H
-KH -> S
-KS -> S
-NH -> K
-SS -> P
-PV -> P
-KV -> V
-ON -> N
-BS -> C
-HP -> K
-SB -> P
-VC -> B
-HB -> N
-FS -> V
-VP -> K
-BB -> N
-FK -> S
-CS -> P
-SO -> F
-HF -> F
-VV -> C
-BC -> S
-SN -> K
-KB -> H
-BN -> H
-HO -> S
-KC -> F
-CP -> S
-HC -> S
-OS -> K
-NK -> N
-BF -> S
-VN -> B
-SK -> K
-HV -> B
-KF -> H
-FV -> B
-VF -> H
-BH -> S
-NN -> O
-HH -> K
-CN -> H
-PH -> V
-NF -> S
-OV -> P
-OC -> V
-OK -> H
-OF -> H
-HK -> N
-FH -> P
-BK -> N
-VS -> H
-NO -> V
-VK -> K
-CF -> N
-CB -> N
-NC -> K
-PK -> B
-VH -> F
-FF -> C
-BV -> P
-OP -> K'''.split('\n')
+s=INPUT1
+t=INPUT2.split('\n')
 t=[j.split(' -> ') for j in t]
 u={i:j for i,j in t}
 for i in range(10):
@@ -117,7 +18,7 @@ for i in range(10):
 t=[s.count(i) for i in set(s)]
 t.sort()
 print(t[-1]-t[0])
-s='OFSVVSFOCBNONHKFHNPK'
+s=INPUT1
 d={}
 for i in range(len(s)-1):
     if s[i:i+2] in d:
